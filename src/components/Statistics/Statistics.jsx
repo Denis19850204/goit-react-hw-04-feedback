9bf128cab087b1fc28624223ae12d19afdad7229
Denis19850204 <dg04021985@gmail.com>
2022-11-18 21:23:33 +0200

    done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,70 +1,58 @@
 import { SecondTitle } from '../SecondTitle/SecondTitle';
 import { StatisticsList } from './StatisticsList';
-import React from 'react';
 import FeedBack from 'components/Feedback/Feedback';
-
 import { useState } from 'react';
 
-export default function Statistics () {
+export default function Statistics() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const options = { good, neutral, bad };
-  console.log(options)
 
-  // handlerBtn = options => {
-  //   this.setState(prevState => ({
-  //     [options]: prevState[options] + 1,
-  //   }));
-  // };
+  const handlerBtn = options => {
+    if (options === 'good') {
+      setGood(good + 1);
+    }
 
-  // countTotalFeedback = () => {
-  //   const { good, neutral, bad } = this.state;
-  //   let total = good + neutral + bad;
+    if (options === 'neutral') {
+      setNeutral(neutral + 1);
+    }
 
-  //   return total;
-  // };
+    if (options === 'bad') {
+      setBad(bad + 1);
+    }
+  };
 
-  // countPositiveFeedbackPercentage = () => {
-  //   const { good } = this.state;
-  //   const total = this.countTotalFeedback();
-  //   let percent = Math.ceil((good * 100) / total);
-  //   return percent;
-  // };
+  const countTotalFeedback = () => {
+    let total = good + neutral + bad;
+    return total;
+  };
 
-  
-    // const { good, neutral, bad } = this.state;
-    return (
-      <div>
-        <FeedBack
-          options={Object.keys(options)}
-          // onLeaveFeedback={this.handlerBtn}
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    let percent = Math.ceil((good * 100) / total);
+    return percent;
+  };
+
+  return (
+    <div>
+      <FeedBack options={Object.keys(options)} onLeaveFeedback={handlerBtn} />
+      <SecondTitle text={'Statistics'} />
+      {countTotalFeedback() ? (
+        <StatisticsList
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          percentage={countPositiveFeedbackPercentage()}
         />
-        <SecondTitle text={'Statistics'} />
-        {/* {this.countTotalFeedback() ? (
-          <StatisticsList
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            percentage={this.countPositiveFeedbackPercentage()}
-          />
-        ) : (
-          <p>There is no feedback</p>
-        )} */}
-      </div>
-    );
-  
+      ) : (
+        <p>There is no feedback</p>
+      )}
+    </div>
+  );
 }
-
-  
-
-
-
-
-
-
 
 // class Statistics extends React.Component {
 //   state = {
